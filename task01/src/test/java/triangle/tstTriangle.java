cbf30e41d8da1d4c9628dedb7799f8e35f824a62
dziyana-trukhan <dziyana.trukhan@mail.ru>
2018-11-29 00:36:52 +0300

    1 commit

---
+++ task01/src/test/java/triangle/tstTriangle.java
@@ -0,0 +1,11 @@
+package triangle;
+
+import org.testng.annotations.Test;
+
+public class tstTriangle {
+    @Test
+    public void tstTest1() {
+        System.out.println("One-tstTest1");
+    }
+
+}
